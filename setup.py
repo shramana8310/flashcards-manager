f89a3bc52d32b49d080fba5b2f9c9e03ce34f929
shramana8310 <shramana8310@gmail.com>
2017-01-03 21:42:50 +0900

    Major functionalities implemented

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
-from cx_Freeze import setup, Executable
 import sys
+from cx_Freeze import setup, Executable
 
 base = 'Win32GUI' if sys.platform == 'win32' else None
-executables = Executable(
-    script='PyFlashcardsManager.pyw',
+
+includes = []
+excludes = []
+
+executable = Executable(
+    script='FlashcardsManager.pyw',
     base=base,
-    shortcutName='Flashcards Manager',
-    shortcutDir='DesktopFolder'
+    icon='res/icon.ico',
 )
-
-build_exe_options = {
-    'include_files': [r'res/'],
-}
-
-bdist_msi_options = {
-    'upgrade_code': '{EF063422-3DDC-4C70-9FB9-1D8591E2D561}'
-}
 
 setup(
     name='Flashcards Manager',
-    version='0.0.1',
-    description='A simple GUI application that generates flashcards',
-    author='shramana',
+    version='1.0.0',
+    description='An application that generates flashcards by looking up dictionaries',
+    author='Shramana',
     options={
-        'build_exe': build_exe_options,
-        'build_msi': bdist_msi_options
+        'build_exe': {
+            'includes': includes,
+            'excludes': excludes,
+        }
     },
-    executables=[executables]
+    executables=[executable],
 )
